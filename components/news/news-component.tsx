a37c2aa7f789d2d3457ac4d358cb56a2505c8e2f
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-02-27 18:48:48 +0100

    -fixed: width of article

--- components/news/news-component.tsx
+++ components/news/news-component.tsx
@@ -7,7 +7,7 @@ const NewsComponent = ({news} : {news: News}) => {
   })
 
   return (
-    <article className='prose w-full'>
+    <article className='prose max-w-none mx-auto w-3/4'>
       <h1>{news.title}</h1>
       <h3>{`${news.author} from ${new Date(news.publishedAt).toLocaleDateString('de-DE')}`}</h3>
       <h4><a href={news.sourceUrl}>{`Source ${news.sourceUrl}`}</a></h4>
