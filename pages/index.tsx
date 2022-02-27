2c295bc41e3a8b4643fd7ba84ca1984f90e095f4
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-02-27 18:04:11 +0100

    -moved: layout into its own container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPageContext } from 'next'
-import Layout from '../components/layout'
+import Layout from '../components/layout/layout'
 import { fetchNewsPage } from '../lib/get-news'
 import { News } from '../types/News';
 import { Page } from '../types/page';
 
 export async function getServerSideProps(context: NextPageContext) {
   const page = 0;
-  const pageSize = 25;
+  const pageSize = 1;
   const newsPage = await fetchNewsPage({page, pageSize});
 
   return {
